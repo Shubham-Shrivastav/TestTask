9ad61a09cc2a456dee1f963ca367b413cfa2ebd7
Shubham-Shrivastav <shuboom969@gmail.com>
2023-12-05 01:21:35 +0530

    Add functionality to show diff icons

--- src/components/Featcher.jsx
+++ src/components/Featcher.jsx
@@ -1,63 +1,73 @@
 import React, { useEffect, useState } from 'react';
-import { FaSpinner } from 'react-icons/fa';
+import { FaSpinner, FaAndroid, FaUserCircle, FaQuestionCircle } from 'react-icons/fa';
 
 const Featcher = () => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        fetch('https://swapi.dev/api/people/')
-            .then((res) => res.json())
-            .then((result) => {
-                setData(result.results);
-                setLoading(false);
-                console.log(result);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                setLoading(false);
-            });
-    }, []);
+  useEffect(() => {
+    fetch('https://swapi.dev/api/people/')
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result.results);
+        setLoading(false);
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        setLoading(false);
+      });
+  }, []);
 
-    return (
-        <>
-            {loading && <h2><FaSpinner /></h2>}
-            {!loading && data && (
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Height</th>
-                                <th>Mass</th>
-                                <th>Gender</th>
-                                <th>Hair Color</th>
-                                <th>Skin Color</th>
-                                <th>Eye Color</th>
-                                <th>Birth Year</th>
-                                <th>Species</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map((person, index) => (
-                                <tr key={index}>
-                                    <td>{person.name}</td>
-                                    <td>{person.height}</td>
-                                    <td>{person.mass}</td>
-                                    <td>{person.gender}</td>
-                                    <td>{person.hair_color}</td>
-                                    <td>{person.skin_color}</td>
-                                    <td>{person.eye_color}</td>
-                                    <td>{person.birth_year}</td>
-                                    <td>{person.species}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            )}
-        </>
-    );
+  return (
+    <>
+      {loading && <h2><FaSpinner /></h2>}
+      {!loading && data && (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Height</th>
+                <th>Mass</th>
+                <th>Gender</th>
+                <th>Hair Color</th>
+                <th>Skin Color</th>
+                <th>Eye Color</th>
+                <th>Birth Year</th>
+                <th>Species</th>
+                <th>Icon</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((person, index) => (
+                <tr key={index}>
+                  <td>{person.name}</td>
+                  <td>{person.height}</td>
+                  <td>{person.mass}</td>
+                  <td>{person.gender}</td>
+                  <td>{person.hair_color}</td>
+                  <td>{person.skin_color}</td>
+                  <td>{person.eye_color}</td>
+                  <td>{person.birth_year}</td>
+                  <td>{person.species}</td>
+                  <td>
+                    {person.species === 'Droid' ? (
+                      <FaAndroid />
+                    ) : person.species === 'Human' ? (
+                      <FaUserCircle />
+                    ) : (
+                      <FaQuestionCircle />
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Featcher;
