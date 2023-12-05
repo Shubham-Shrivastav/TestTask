7183ec5ac00f4687b2c916ca3f031f7d5f30d464
Shubham-Shrivastav <shuboom969@gmail.com>
2023-12-06 03:04:28 +0530

    Add the theme to overright the fonts in App

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './components/Theme/Theme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
