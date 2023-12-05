82b3b95256fc1e3a21d4c8bc1fddfb020055e51d
Shubham-Shrivastav <shuboom969@gmail.com>
2023-12-06 00:58:31 +0530

    Correct the icon with pagination as API responce

--- src/components/Featcher.jsx
+++ src/components/Featcher.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { FaSpinner, FaAndroid, FaUserCircle, FaQuestionCircle } from 'react-icons/fa';
+import {
+    FaSpinner,
+    FaAndroid,
+    FaUserCircle,
+    FaQuestionCircle,
+} from 'react-icons/fa';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,10 +14,11 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { TextField, Box, Button } from '@mui/material';
+
 const columns = [
     { id: 'name', name: 'Name' },
     { id: 'height', name: 'Height' },
-    { id: 'Mass', name: 'Mass' },
+    { id: 'mass', name: 'Mass' },
     { id: 'gender', name: 'Gender' },
     { id: 'hair_color', name: 'Hair Color' },
     { id: 'skin_color', name: 'Skin Color' },
@@ -21,41 +27,7 @@ const columns = [
     { id: 'species', name: 'Species' },
     { id: 'icon', name: 'Icon' },
 ];
-const speciesIcon = (species) => {
-    switch (species) {
-        case 'Droid':
-            return <FaAndroid />;
-        case 'Human':
-            return <FaUserCircle />;
-        default:
-            return <FaQuestionCircle />;
-    }
-};
-function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
-function getComparator(order, orderBy) {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
-function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) {
-            return order;
-        }
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-}
+
 const Featcher = () => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -64,40 +36,78 @@ const Featcher = () => {
     const [order, setOrder] = useState('asc');
     const [orderBy, setOrderBy] = useState('');
     const [search, setSearch] = useState('');
+
     const handleSearchChange = (event) => {
         setSearch(event.target.value.toLowerCase());
     };
+
     const handleSearch = () => {
         setLoading(true);
-        fetch(`https://swapi.dev/api/people/?search=${search}`)
-            .then((res) => res.json())
-            .then((result) => {
-                setData(result.results);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                setLoading(false);
-            });
     };
+
+    useEffect(() => {
+        const fetchAllData = async () => {
+            setLoading(true);
+            let allData = [];
+            let nextPage = 'https://swapi.dev/api/people/';
+
+            while (nextPage) {
+                const result = await fetch(nextPage);
+                const data = await result.json();
+                allData = allData.concat(data.results);
+
+                nextPage = data.next;
+            }
+
+            const charactersWithSpecies = await Promise.all(
+                allData.map(async (character) => {
+                    if (character.species.length > 0) {
+                        const speciesResult = await fetch(character.species[0]);
+                        const speciesData = await speciesResult.json();
+                        return { ...character, speciesData };
+                    }
+                    return character;
+                })
+            );
+
+            setData(charactersWithSpecies);
+            setLoading(false);
+        };
+
+        fetchAllData();
+    }, []);
+
     const handleSortRequest = (property) => {
         const isAsc = orderBy === property && order === 'asc';
         setOrder(isAsc ? 'desc' : 'asc');
         setOrderBy(property);
     };
-    useEffect(() => {
-        fetch('https://swapi.dev/api/people/')
-            .then((res) => res.json())
-            .then((result) => {
-                setData(result.results);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                setLoading(false);
-            });
-    }, []);
-    const sortedData = stableSort(data, getComparator(order, orderBy));
+
+    const speciesIcon = (speciesData) => {
+        if (speciesData) {
+            switch (speciesData.name) {
+                case 'Droid':
+                    return <FaAndroid />;
+                case 'Human':
+                    return <FaUserCircle />;
+                default:
+                    return <FaQuestionCircle />;
+            }
+        }
+        return <FaQuestionCircle />;
+    };
+
+    const sortedData = data.sort((a, b) => {
+        const isAsc = order === 'asc';
+        if (a[orderBy] < b[orderBy]) {
+            return isAsc ? -1 : 1;
+        }
+        if (a[orderBy] > b[orderBy]) {
+            return isAsc ? 1 : -1;
+        }
+        return 0;
+    });
+
     return (
         <>
             {loading && <h2><FaSpinner /></h2>}
@@ -143,7 +153,7 @@ const Featcher = () => {
                                             {columns.map((column) => (
                                                 <TableCell key={column.id}>
                                                     {column.id === 'icon'
-                                                        ? speciesIcon(person[column.id])
+                                                        ? speciesIcon(person.speciesData)
                                                         : person[column.id]}
                                                 </TableCell>
                                             ))}
@@ -169,5 +179,5 @@ const Featcher = () => {
         </>
     );
 };
-export default Featcher;
 
+export default Featcher;
