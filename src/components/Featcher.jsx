c4ef362b158a519aaaaee60ff3386b498e98b202
Shubham-Shrivastav <shuboom969@gmail.com>
2023-12-08 12:02:11 +0530

    WIP:Add searching

--- src/components/Featcher.jsx
+++ src/components/Featcher.jsx
@@ -1,68 +1,92 @@
-import React, { useEffect, useState } from 'react';
-import { FaSpinner, FaAndroid, FaUserCircle, FaQuestionCircle } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
+import { FaAndroid, FaUserCircle, FaQuestionCircle, FaSpinner } from 'react-icons/fa';
 import { DataGrid } from '@mui/x-data-grid';
 
-const columns = [
-    { field: 'name', headerName: 'Name', width: 150 },
-    { field: 'height', headerName: 'Height', width: 120 },
-    { field: 'mass', headerName: 'Mass', width: 120 },
-    { field: 'gender', headerName: 'Gender', width: 120 },
-    { field: 'hair_color', headerName: 'Hair Color', width: 150 },
-    { field: 'skin_color', headerName: 'Skin Color', width: 150 },
-    { field: 'eye_color', headerName: 'Eye Color', width: 150 },
-    { field: 'birth_year', headerName: 'Birth Year', width: 150 },
-    { field: 'species', headerName: 'Species', width: 120 },
-    {
-        field: 'icon',
-        headerName: 'Icon',
-        width: 100,
-        renderCell: (params) => {
-            const species = params.row.species;
-            return (
-                <span>
-                    {species === 'Droid' ? <FaAndroid /> : species === 'Human' ? <FaUserCircle /> : <FaQuestionCircle />}
-                </span>
-            );
-        },
-    },
-];
-
 const Featcher = () => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [tableData, setTableData] = useState({ count: 0, next: null, previous: null, results: [] });
+    const [url, setUrl] = useState('https://swapi.dev/api/people/');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('https://swapi.dev/api/people/')
-            .then((res) => res.json())
-            .then((result) => {
-                const updatedData = result.results.map((item, index) => ({
-                    id: index + 1,
-                    ...item,
-                }));
-                setData(updatedData);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                setLoading(false);
-            });
-    }, []);
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch');
+                }
+                const data = await response.json();
+                setTableData({ ...data, rows: data.results });
+                setError(null);
+            } catch (err) {
+                setError('Error fetching data');
+            }
+        };
+
+        fetchData();
+    }, [url]);
+
+    const columns = [
+        { field: 'name', headerName: 'Name', width: 150 },
+        { field: 'height', headerName: 'Height', width: 120 },
+        { field: 'mass', headerName: 'Mass', width: 120 },
+        { field: 'gender', headerName: 'Gender', width: 120 },
+        { field: 'hair_color', headerName: 'Hair Color', width: 150 },
+        { field: 'skin_color', headerName: 'Skin Color', width: 150 },
+        { field: 'eye_color', headerName: 'Eye Color', width: 150 },
+        { field: 'birth_year', headerName: 'Birth Year', width: 150 },
+        { field: 'species', headerName: 'Species', width: 120 },
+        {
+            field: 'icon',
+            headerName: 'Icon',
+            width: 100,
+            renderCell: (params) => {
+                const species = params.row.species;
+                return (
+                    <span>
+                        {species === 'Droid' ? <FaAndroid /> : species === 'Human' ? <FaUserCircle /> : <FaQuestionCircle />}
+                    </span>
+                );
+            },
+        },
+    ];
+
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    const handlePageChange = (newPage) => {
+        setUrl(`https://swapi.dev/api/people/?page=${newPage + 1}`);
+    };
+
+    const getRowId = (row) => row.name;
+
+    const filteredData = tableData.results.filter((row) =>
+        row.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
-        <>
-            {loading && <h2><FaSpinner /></h2>}
-            {!loading && data && (
-                <div style={{ height: 400, width: '100%' }}>
-                    <DataGrid
-                        rows={data}
-                        columns={columns}
-                        pageSize={10}
-                        rowsPerPageOptions={[10]}
-                        autoHeight
-                    />
-                </div>
-            )}
-        </>
+        <div>
+            {error && <p>{error}</p>}
+            <input
+                type="text"
+                placeholder="Search by name"
+                value={searchTerm}
+                onChange={handleSearch}
+            />
+            <DataGrid
+                rows={filteredData}
+                columns={columns}
+                pagination
+                pageSize={10}
+                rowCount={tableData.count}
+                paginationMode="server"
+                onPageChange={handlePageChange}
+                pageSizeOptions={[10, 15, 20]}
+                getRowId={getRowId}
+            />
+            {!tableData.count && <h2><FaSpinner /></h2>}
+        </div>
     );
 };
 
